e533df58d9a5de39971f4587425fe8c37f258549
fbc <849067797@qq.com>
2019-11-16 10:25:17 +0800

    mqtt ok

---
+++ wifi_lwip_mqtt_baidu_dht11_drive/User/main.c
@@ -0,0 +1,222 @@
+#include "wwd_network.h"
+#include "wwd_management.h"
+#include "wwd_wifi.h"
+#include "wwd_debug.h"
+#include "wwd_assert.h"
+#include "platform/wwd_platform_interface.h"
+#include "RTOS/wwd_rtos_interface.h"
+#include "wwd_buffer_interface.h"
+
+
+#include "wifi_base_config.h"
+
+/* FreeRTOS头文件 */
+#include "FreeRTOS.h"
+#include "task.h"
+/* 开发板硬件bsp头文件 */
+
+#include "./usart/bsp_debug_usart.h"
+
+#include "platform_init.h"
+#include "./led/bsp_led.h"
+
+#include <cm_backtrace.h>
+#define HARDWARE_VERSION               "V1.0.0"
+#define SOFTWARE_VERSION               "V0.1.0"
+
+
+/** @endcond */
+
+/*****************************************************
+ *               静态函数声明
+ ******************************************************/
+
+static void tcpip_init_done( void * arg );
+static void startup_thread( void *arg );
+
+/******************************************************
+ *               变量定义
+ ******************************************************/
+
+static TaskHandle_t  startup_thread_handle;
+void BSP_Init();
+
+
+/**
+ * 主函数
+ */
+
+int main( void )
+{
+		BSP_Init();
+
+    /*创建一个初始线程 */									
+		BaseType_t xReturn = pdPASS;
+		xReturn = xTaskCreate((TaskFunction_t )startup_thread,  /* 任务入口函数 */
+													(const char*    )"app_thread",/* 任务名字 */
+													(uint16_t       )APP_THREAD_STACKSIZE/sizeof( portSTACK_TYPE ),  /* 任务栈大小 */
+													(void*          )NULL,/* 任务入口函数参数 */
+													(UBaseType_t    )DEFAULT_THREAD_PRIO, /* 任务的优先级 */
+													(TaskHandle_t*  )&startup_thread_handle);/* 任务控制块指针 */ 
+		 /* 启动任务调度 */           
+		if(pdPASS == xReturn)
+			vTaskStartScheduler();   /* 启动任务，开启调度 */
+		else
+			return -1;  
+    /* 除非vTaskStartScheduler中出现错误，否则永远不要到这里 */
+    WPRINT_APP_ERROR(("Main() function returned - error" ));
+    return 0;
+}
+
+
+
+/**
+ *  初始线程功能-启动LwIP并调用app_main
+ * 该函数使用tcpip_init函数启动LwIP，然后等待信号量，
+ * 直到LwIP通过调用回调@ref tcpip_init_done指示其已
+ * 启动。完成后，将调用应用程序的@ref app_main函数。
+ * @param arg :  未使用-符合线程功能原型所需
+ */
+
+static void startup_thread( void *arg )
+{
+    SemaphoreHandle_t lwip_done_sema;
+    UNUSED_PARAMETER( arg);
+
+   // WPRINT_APP_INFO( ( "\nPlatform " PLATFORM " initialised\n" ) );
+    WPRINT_APP_INFO( ( "Started FreeRTOS" FreeRTOS_VERSION "\n" ) );
+    WPRINT_APP_INFO( ( "Starting LwIP " LwIP_VERSION "\n" ) );
+
+    /* 创建信号量以在LwIP完成初始化时发出信号 */
+    lwip_done_sema = xSemaphoreCreateCounting( 1, 0 );
+    if ( lwip_done_sema == NULL )	
+    {
+        /*无法创建信号量 */
+        WPRINT_APP_ERROR(("Could not create LwIP init semaphore"));
+        return;
+    }
+
+    /*初始化LwIP，提供回调函数和回调信号量 */
+    tcpip_init( tcpip_init_done, (void*) &lwip_done_sema );
+    xSemaphoreTake( lwip_done_sema, portMAX_DELAY );
+    vQueueDelete( lwip_done_sema );
+
+    /* 运行主应用程序功能 */
+    app_main( );
+
+    /* 清理此启动线程*/
+    vTaskDelete( startup_thread_handle );
+}
+
+
+
+/**
+ *  LwIP初始化完成回调
+ * @param arg : the handle for the semaphore to post (cast to a void pointer)
+ */
+
+static void tcpip_init_done( void * arg )
+{
+    SemaphoreHandle_t * lwip_done_sema = (SemaphoreHandle_t *) arg;
+    xSemaphoreGive( *lwip_done_sema );
+}
+
+
+/***********************************************************************
+  * @ 函数名  ： BSP_Init
+  * @ 功能说明： 板级外设初始化，所有板子上的初始化均可放在这个函数里面
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  *********************************************************************/
+static void SystemClock_Config(void);
+static void BSP_Init(void)
+{
+
+	SystemClock_Config();
+  
+	HAL_SYSTICK_Config( HAL_RCC_GetSysClockFreq() / configTICK_RATE_HZ );	
+	
+	platform_init_mcu_infrastructure();  
+	
+	/* usart 端口初始化 */
+  DEBUG_USART_Config();
+	
+			  /* CmBacktrace initialize */
+  cm_backtrace_init("F767", HARDWARE_VERSION, SOFTWARE_VERSION);
+	
+	LED_GPIO_Config();
+
+
+  
+}
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下 : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 216000000
+  *            HCLK(Hz)                       = 216000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 432
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 9
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 7
+  * @param  无
+  * @retval 无
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  HAL_StatusTypeDef ret = HAL_OK;
+
+  /* 使能HSE，配置HSE为PLL的时钟源，配置PLL的各种分频因子M N P Q 
+	 * PLLCLK = HSE/M*N/P = 25M / 25 *432 / 2 = 216M
+	 */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 432;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 9;
+  
+  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+  
+  /* 激活 OverDrive 模式以达到216M频率  */  
+  ret = HAL_PWREx_EnableOverDrive();
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+  
+  /* 选择PLLCLK作为SYSCLK，并配置 HCLK, PCLK1 and PCLK2 的时钟分频因子 
+	 * SYSCLK = PLLCLK     = 216M
+	 * HCLK   = SYSCLK / 1 = 216M
+	 * PCLK2  = SYSCLK / 2 = 108M
+	 * PCLK1  = SYSCLK / 4 = 54M
+	 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2; 
+  
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }  
+}
+/*********************************************END OF FILE**********************/
+

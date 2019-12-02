e4397cb00c75e1ec44806fbd621f3ad0fa5795e5
fbc <849067797@qq.com>
2019-12-02 09:51:22 +0800

    删除无用代码 开始 写 双buf处理

--- wifi_lwip_tcpecho_socket-camera/User/cammera_middleware.c
+++ wifi_lwip_tcpecho_socket-camera/User/cammera_middleware.c
@@ -249,6 +249,26 @@ void DCMI_Stop(void)
 
 
 
+
+
+#if 1
+
+/*帧中断实现*/
+void DCMI_IRQHandler_Funtion(void)
+{
+	
+	camera_data *data_p;	
+
+	frame_counter ++;
+	
+		HAL_DCMI_Stop(&DCMI_Handle) ;
+	/*配置DMA传输*/
+	HAL_DCMI_Start_DMA(&DCMI_Handle, DCMI_MODE_CONTINUOUS, (uint32_t )data_p->head, CAMERA_QUEUE_DATA_LEN);
+
+	
+}
+
+#else 
 /*帧中断实现*/
 void DCMI_IRQHandler_Funtion(void)
 {
@@ -263,4 +283,6 @@ void DCMI_IRQHandler_Funtion(void)
 	dma_complete_counter=0;
 	
 }
+#endif
+
 
